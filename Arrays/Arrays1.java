236a03bd9e6062aa5cd4fe8524f1e273bb4224a7
ans_sharma <ak.sharma4918@gmail.com>
2022-11-02 11:28:54 +0530

    first commit

---
+++ Arrays/Arrays1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+// 2022-11-02 11:15:29
+// Reverse the array 
+public class Arrays1 {
+    public static void main(String[] args) {
+        int arr[] = new int[] { 7, 5, 6, 21, 8, 6 };
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[(arr.length-1) - i];
+            arr[(arr.length-1) - i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ Arrays/Arrays2.java
@@ -0,0 +1,7 @@
+// 2022-11-02 11:28:22
+// Find the maximum and minimum element in an array
+public class Arrays2 {
+    public static void main(String[] args) {
+             
+    }
+}
